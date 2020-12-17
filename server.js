b0d5668e2d7ed8dfd2a2197eac52a89803b01326
Raymond SO <raymond.wc.so@gmail.com>
2020-12-17 13:44:49 +0800

    updated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/',function(req,res) {
-  res.send('Node version: ' + process.version);
+app.get('/', (req,res) => {
+  res.status(200).end(`Node version: ${process.version}\n`);
 });
 
 app.listen(process.env.PORT || 8099);
