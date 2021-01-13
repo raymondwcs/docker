d31d433b67de791bccb6e4fba55e66cd478ea783
Raymond SO <raymond.wc.so@gmail.com>
2021-01-13 19:24:07 +0800

    updated

--- jwt/dbseed/import.sh
+++ jwt/dbseed/import.sh
@@ -1,2 +1,1 @@
-#mongoimport --uri 'mongodb://root:password@mongo:27017/test?authMechanism=DEFAULT&authSource=admin' --collection books --file books.json --jsonArray
 mongoimport --host mongo:27017 --authenticationDatabase admin --username root --password password --collection books --jsonArray --file '/usr/src/app/books.json' 
